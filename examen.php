553394e1f4e170427f0c58b8310ce7775e566b7a
pitufina3 <pitufina3@.com>
2018-05-02 12:14:32 +0200

    first commit

---
+++ examen.php
@@ -0,0 +1,181 @@
+class="text-center">
+
+        <h1>Ofertas de empleo</h1>
+
+        <p>Mercadona dispone de un Modelo de Recursos Humanos 
+
+            reconocido por ofrecer puestos de trabajo estables y de calidad.
+
+        </p>
+
+        <button type="button" class="btn btn-md">Ofertas de empleo</button>
+
+    </div>
+
+
+
+    <div id="logo-footer" class="text-center">
+
+        <img src="mercadona_logo_footer.jpg" alt="Mercadona" width="560px">
+
+    </div>
+
+
+
+    <footer>
+
+        <div class="row">
+
+            <div class="col">
+
+                <p><strong>Lo más buscado </strong></p>
+
+                <ul>
+
+                    <li>Localizar supermercados</li>
+
+                    <li>Trabaja con nosotros</li>
+
+                    <li>Sala de prensa</li>
+
+                    <li>Conoce Mercadona</li>
+
+                    <li>Consejos</li>
+
+                </ul>
+
+            </div>
+
+            <div class="col">
+
+                <p><strong>Enlaces de interés </strong></p>
+
+                <ul>
+
+                    <li>Compra online</li>
+
+                    <li>Tarjeta Mercadona</li>
+
+                    <li>Factura clientes</li>
+
+                    <li>Portal trabajador</li>
+
+                    <li>E-factura proveedores</li>
+
+                    <li>Portal Trinidad</li>
+
+                </ul>
+
+            </div>
+
+            <div class="col">
+
+                <p><strong>Consejos </strong></p>
+
+                <ul>
+
+                    <li>Consejos de alimentación</li>
+
+                    <li>Consejos de perfumería</li>
+
+                    <li>Consejos de hogar</li>
+
+                    <li>Consejos de mascotas</li>
+
+                </ul>
+
+            </div>
+
+            <div class="col">
+
+                <p><strong>Atención al cliente </strong></p>
+
+                <ul>
+
+                    <li><a href=""><i class="fas fa-envelope"></i> Contacto</a></li>
+
+                    <li><i class="fas fa-phone"></i> 900 500 103</li>
+
+                </ul>
+
+                <p><strong>Redes sociales</strong></p>
+
+                <p>
+
+                    <i class="fab fa-facebook"></i> 
+
+                    <i class="fab fa-twitter"></i> 
+
+                    <i class="fab fa-youtube"></i> 
+
+                    <i class="fab fa-instagram"></i>
+
+                </p>
+
+                
+
+            </div>
+
+            <div class="col">
+
+                <img src="corredor_med.jpg" alt="Corredor Mediterráneo">
+
+            </div>
+
+        
+
+        </div>
+
+        <div class="row">
+
+            <div class="col-md-4">
+
+                <p>© Mercadona S.A. A46103834. Todos los derechos reservados.</p>
+
+            </div>
+
+            <div class="col-md-8">
+
+                <ul>
+
+                    <li>Accesibilidad</li>
+
+                    <li>Condiciones Wifi</li>
+
+                    <li>Política de privacidad</li>
+
+                    <li>Política de cookies</li>
+
+                    <li>Términos y condiciones</li>
+
+                    <li>Requisitos técnicos</li>
+
+                </ul>
+
+            </div>
+
+        </div>
+
+    </footer>
+
+
+
+
+
+    <!-- Optional JavaScript -->
+
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+
+    
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+    <script src="app.js"></script>
+
+</body>
+
+</html>
